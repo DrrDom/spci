277c62a56355d25a03d4fc89ea2df98755962fb0
Pavel Polishchuk <pavel_polishchuk@ukr.net>
2018-06-07 14:03:07 +0200

    Improved command line arguments and changed behavior when output file is omitted in add_relative_frag_size.py

--- utils/add_relative_frag_size.py
+++ utils/add_relative_frag_size.py
@@ -1,16 +1,26 @@
+#==============================================================================
+# author          : Maria Matveiva
+# date            : 07-06-2018
+# version         : 0.1
+# python_version  : 3.5.2
+# copyright       : Maria Matveieva 2018
+# license         : GPL3
+#==============================================================================
+
 from rdkit import Chem
 import argparse
 import re
+import shutil
 
 
 def main_params(contrib_fname, inp_format, out_fname, sd_fname):
     mol_dict = load_mol_dict(sd_fname)
     if inp_format == "wide":
         fr_dict, fr_lst = load_frags_wide(contrib_fname)
-        append_wide(contrib_fname, fr_dict, fr_lst, mol_dict)
+        write_wide(contrib_fname, fr_dict, fr_lst, mol_dict, out_fname)
     else:
         fr_dict = load_frags_long(contrib_fname)
-        append_long(contrib_fname, out_fname, fr_dict, mol_dict)
+        write_long(contrib_fname, out_fname, fr_dict, mol_dict)
 
 
 def load_mol_dict(sd_fname):
@@ -45,7 +55,7 @@ def load_frags_long(contrib_fname):
     return fr_dict
 
 
-def append_wide(contrib_fname, fr_dict, fr_lst, mol_dict):
+def write_wide(contrib_fname, fr_dict, fr_lst, mol_dict, out_fname):
 
     m = ["mol_size"]
     f = ["frag_size"]
@@ -55,14 +65,20 @@ def append_wide(contrib_fname, fr_dict, fr_lst, mol_dict):
         m.append(mol_dict[mol])
         f.append(fr_dict[frag])
         p.append(round(fr_dict[frag] / mol_dict[mol], 2))
-    
-    with open(contrib_fname, "a") as contr_file:
-        contr_file.write("\t".join(map(str, m)) + "\n")
-        contr_file.write("\t".join(map(str, f)) + "\n")
-        contr_file.write("\t".join(map(str, p)) + "\n")
+    if out_fname is not None:
+        shutil.copyfile(contrib_fname, out_fname)
+        with open(out_fname, "a") as out:
+            out.write("\t".join(map(str, m)) + "\n")
+            out.write("\t".join(map(str, f)) + "\n")
+            out.write("\t".join(map(str, p)) + "\n")
+    else:
+        with open(contrib_fname, "a") as contr_file:
+            contr_file.write("\t".join(map(str, m)) + "\n")
+            contr_file.write("\t".join(map(str, f)) + "\n")
+            contr_file.write("\t".join(map(str, p)) + "\n")
 
 
-def append_long(contrib_fname, out_fname, fr_dict, mol_dict):
+def write_long(contrib_fname, out_fname, fr_dict, mol_dict):
     with open(out_fname, "wt") as out:
         with open(contrib_fname, "rt") as contr_file:
             out.write(contr_file.readline().strip() + "\t" + "\t".join(["mol_size", "frag_size", "relative_frag_size"]) + "\n")
@@ -85,9 +101,9 @@ def main():
                         help='format of input file wide/long. Default: wide.')
     parser.add_argument('-o', '--output', metavar='output_contributions.txt', default=None,
                         help='Output text file, will contain same data as input plus added cols with calculated data.'
-                             'Required only for long format(otherwise will not be created: new data will be appended '
-                             'to input file).')
-    parser.add_argument('--sdf', metavar='mols.sdf', required=True,
+                             'Required for long format. For wide format it can be omitted, in this case new rows '
+                             'will be appended to the input file with contributions).')
+    parser.add_argument('-s','--sdf', metavar='input.sdf', required=True,
                         help='input sdf with molecules from which frags were derived. Should contain molecule titles '
                              'identical to those used in the input file with contributions.')
 
